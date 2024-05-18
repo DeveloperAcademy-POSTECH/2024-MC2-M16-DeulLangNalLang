bbddc8c9cf8eb4adf7f54acd76b89586d7965808
ryumbrella <sani9511@naver.com>
2024-05-18 21:04:23 +0900

    feat: 상장 내 텍스트 레이아웃

--- DeulLangNalLang/DeulLangNalLang/AwardDetailView.swift
+++ DeulLangNalLang/DeulLangNalLang/AwardDetailView.swift
@@ -14,10 +14,48 @@ struct AwardDetailView: View {
     var body: some View {
         VStack(spacing: 0){
             Spacer()
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 329, height: 480)
-                .foregroundColor(Color(red: 26/255, green: 195/255, blue: 69/255))
-                .padding(.bottom, 59)
+            ZStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 329, height: 480)
+                    .foregroundColor(Color(red: 26/255, green: 195/255, blue: 69/255))
+                VStack(spacing: 0){
+                    Rectangle()
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.gray)
+                        .padding(20)
+                    Spacer()
+                }
+                VStack(spacing: 0){
+                    Spacer()
+                    HStack(spacing: 0){
+                        Text("우리집 요리왕")
+                        Text("상")
+                    }
+                    .fontWeight(.heavy)
+                    .font(.system(size: 29))
+                    
+                    
+                    Text("산이는 부모님이 집에 안계실 때 부모님을 대신하여 들이가 배고픈지 물어보고 맛있는 밥을 만들어 주었기에 이 상을 수여합니다.")
+                        .frame(width: 233, height: 233)
+                        .padding(.vertical, 12)
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .multilineTextAlignment(.center)
+                    Text("2024년 5월 5일")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 16))
+                    Text("류들")
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        .padding(.top, 5)
+                        .padding(.bottom, 22)
+
+
+                    
+                }
+            }
+            .frame(width: 329, height: 480)
+            .padding(.bottom, 59)
         }
         .navigationTitle("상장과 자랑 보기")
         .navigationBarBackButtonHidden(true)
