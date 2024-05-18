7f79ca320dd47e1b1e9ac9bfb55b0a519a35155b
LEMONY <jin.lee1028@gmail.com>
2024-05-18 18:48:51 +0900

    AwardView 탭바 기본골격

---
+++ DeulLangNalLang/DeulLangNalLang/AwardMainView.swift
@@ -0,0 +1,54 @@
+//
+//  AwardMainView.swift
+//  DeulLangNalLang
+//
+//  Created by JIN LEE on 5/18/24.
+//
+
+import SwiftUI
+
+struct AwardMainView: View {
+        var body: some View {
+            TabView {
+                TabSub1View() // 첫 번째 탭 화면
+                    .tabItem { // 탭바 버튼으로 등록
+                        Image(systemName: "1.circle")
+                        Text("첫 번째 탭바 버튼")
+                    }
+                
+                TabSub2View() // 두 번째 탭 화면
+                    .tabItem { // 탭바 버튼으로 등록
+                        Image(systemName: "2.circle")
+                        Text("두 번째 탭바 버튼")
+                    }
+            }
+            .navigationTitle("탭 바 전환")
+        }
+    }
+
+    struct TabSub1View: View { // 첫 번째 탭 화면
+        var body: some View {
+            VStack {
+                Image(systemName: "1.circle")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding()
+                Text("첫 번째 탭")
+            }
+        }
+    }
+
+    struct TabSub2View: View { // 두 번째 탭 화면
+        var body: some View {
+            VStack {
+                Image(systemName: "2.circle")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding()
+                Text("두 번째 탭")
+            }
+        }
+    }
+#Preview {
+    AwardMainView()
+}
