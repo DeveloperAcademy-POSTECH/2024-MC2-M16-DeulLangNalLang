37ae8225ea3c77cbf3d6eb9c21be4a67e6c03ece
ryumbrella <sani9511@naver.com>
2024-05-18 17:05:05 +0900

    feat: 상단 툴바

--- DeulLangNalLang/DeulLangNalLang/ContentView.swift
+++ DeulLangNalLang/DeulLangNalLang/ContentView.swift
@@ -9,11 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+<<<<<<< dd68db4aef2f7d7fb947626b24c5f637638155fd
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("PRTest")
+=======
+        NavigationStack{
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    .padding(.bottom, 1)
+                NavigationLink(destination: AwardDetailView()){
+                    Text("상장 박물관 메인")
+                }
+            }
+>>>>>>> feat: 상단 툴바
         }
         .padding()
     }
