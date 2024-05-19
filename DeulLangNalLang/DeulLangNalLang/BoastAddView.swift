224bc9765c0b273bc75821ee6e66cfd06754178e
ryumbrella <sani9511@naver.com>
2024-05-19 17:31:33 +0900

    feat: 모달에 텍스트 작성 기능 및 카메라 구동 툴바 추가

--- DeulLangNalLang/DeulLangNalLang/BoastAddView.swift
+++ DeulLangNalLang/DeulLangNalLang/BoastAddView.swift
@@ -1,8 +1,125 @@
-//
-//  BoastAddView.swift
-//  DeulLangNalLang
-//
-//  Created by  SAN RYU on 5/19/24.
-//
+import SwiftUI
+import UIKit
 
-import Foundation
+struct BoastAddView: View {
+    @State private var text = ""
+    @State private var isShowingImagePicker = false
+    @State private var selectedImage: UIImage? = nil
+    @Environment(\.presentationMode) var presentationMode // presentationMode 환경 변수 추가
+
+    var body: some View {
+            NavigationView { // NavigationView로 감싸기
+                VStack {
+                    CustomTextView(text: $text, isShowingImagePicker: $isShowingImagePicker)
+                        .frame(height: 200)
+                        .padding()
+                    
+                    Spacer()
+                    
+                    if let selectedImage = selectedImage {
+                        Image(uiImage: selectedImage)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 200)
+                            .padding()
+                    }
+                }
+                .navigationBarTitle("자랑쓰기")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarItems(trailing: Button("완료") { // 오른쪽에 "완료" 버튼 추가
+                                presentationMode.wrappedValue.dismiss() // 버튼이 눌리면 모달 닫기
+                            })                .sheet(isPresented: $isShowingImagePicker) {
+                    ImagePicker(selectedImage: $selectedImage)
+                }
+            }
+        }
+    }
+
+struct CustomTextView: UIViewRepresentable {
+    @Binding var text: String
+    @Binding var isShowingImagePicker: Bool
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> UITextView {
+        let textView = UITextView()
+        textView.delegate = context.coordinator
+        textView.font = UIFont.systemFont(ofSize: 16)
+        
+        // 툴바 설정
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let cameraButton = UIBarButtonItem(barButtonSystemItem: .camera, target: context.coordinator, action: #selector(context.coordinator.cameraButtonTapped))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.items = [flexibleSpace, cameraButton]
+        textView.inputAccessoryView = toolbar
+        
+        return textView
+    }
+
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.text = text
+    }
+
+    class Coordinator: NSObject, UITextViewDelegate {
+        var parent: CustomTextView
+
+        init(_ parent: CustomTextView) {
+            self.parent = parent
+        }
+
+        func textViewDidChange(_ textView: UITextView) {
+            parent.text = textView.text
+        }
+        
+        @objc func cameraButtonTapped() {
+            parent.isShowingImagePicker = true
+        }
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) private var presentationMode
+    @Binding var selectedImage: UIImage?
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BoastAddView()
+    }
+}
