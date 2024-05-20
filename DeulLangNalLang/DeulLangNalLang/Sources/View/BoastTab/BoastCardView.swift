2ce9ce4f5f4f239e68f99cf901966a93dbf9f7c9
HyunJaeyeon <bomul0109@gmail.com>
2024-05-20 22:25:19 +0900

    feat: 삭제 버튼 클릭 시 ActionSheet 띄우기

--- DeulLangNalLang/DeulLangNalLang/Sources/View/BoastTab/BoastCardView.swift
+++ DeulLangNalLang/DeulLangNalLang/Sources/View/BoastTab/BoastCardView.swift
@@ -3,13 +3,14 @@ import SwiftUI
 struct BoastCardView: View {
     @Binding var tempBoast: Boast
     
-    @State private var Username = "San"
+    @State private var Username = "Deul"
     @State private var images: [String] = [
         "https://i.pinimg.com/564x/1e/a8/1f/1ea81fe0ddc6b0dbd76899c7aebfb47c.jpg",
         "https://i.pinimg.com/564x/8a/be/9b/8abe9b3640dbef426f6c9c9a67457e9d.jpg"
     ]
     
     @State private var showSheet: Bool = false
+    @State private var showActionSheet: Bool = false
     
     var body: some View {
         VStack {
@@ -62,11 +63,14 @@ struct BoastCardView: View {
                         .font(.bodyRegular)
                         .foregroundStyle(.white)
                     Spacer()
+                    
                     /// 본인일 때 (내가 쓴 글) 수정하기 버튼
                     if Username == "Deul" {
                         Menu {
                             Button("수정하기", action:{})
-                            Button(role: .destructive, action:{}) {
+                            Button(role: .destructive, action:{
+                                showActionSheet.toggle()
+                            }) {
                                 Label("삭제하기", systemImage: "trash")
                             }
                             .foregroundColor(.red)
@@ -74,7 +78,9 @@ struct BoastCardView: View {
                             Image(systemName: "ellipsis")
                                 .foregroundStyle(.white)
                         }
+                        .actionSheet(isPresented: $showActionSheet, content: getActionSheet)
                     }
+                    
                     /// 상대일 때 상장주기 버튼
                     if Username == "San"  {
                         Button(action: {
@@ -101,6 +107,19 @@ struct BoastCardView: View {
         //                .frame(height: 324)
         .background(Username == "Deul" ? Color.DNGreen : Color.DNBlue)
         .cornerRadius(16)
+    }
+    
+    //MARK: ActionSheet 동작 함수
+    func getActionSheet() -> ActionSheet {
+        
+        let deleteButton: ActionSheet.Button = .destructive(Text("삭제"))
+        let cancelButton: ActionSheet.Button = .cancel(Text("취소"))
+        
+        let title = Text("정말로 삭제하실 건가요?\n이 작업은 취소할 수 없습니다.")
+        
+        return ActionSheet(title: title,
+                           message: nil,
+                           buttons: [deleteButton, cancelButton])
     }
 }
 
