627f33b27a587c8c9d4f74cababd580ea07c1d0a
HyunJaeyeon <bomul0109@gmail.com>
2024-05-21 00:46:59 +0900

    feat: Trackable Scrollview with header 적용

--- DeulLangNalLang/DeulLangNalLang/Sources/View/BoastTab/BoastMainView.swift
+++ DeulLangNalLang/DeulLangNalLang/Sources/View/BoastTab/BoastMainView.swift
@@ -9,45 +9,93 @@ struct BoastMainView: View {
         Boast(contents: "저녁이 되자 해가 지면서 하늘이 붉게 물들었고, 우리는 그 광경을 바라보며 하루를 마무리했습니다." , date: Date())]
     
     var body: some View {
-        VStack{
-            HStack {
-                Spacer()
-                Menu {
-                    Button("산이만 보기", action:{})
-                    Button("들이만 보기", action:{})
-                    Button("전체보기", action:{})
-                } label: {
-                    Image(systemName: "line.3.horizontal.decrease.circle")
-                        .foregroundStyle(.black)
-                        .font(.title1Regular)
-                }
-                .padding(.trailing, 4)
-                
-                Button(action: {
-                    showSheet.toggle()
-                }) {
-                    Image(systemName: "plus")
-                        .foregroundStyle(.black)
-                        .font(.title1Regular)
-                }
-                .sheet(isPresented: $showSheet) {
-                    BoastAddView()
-                }
-            }
-            .padding(.trailing)
-            .padding(.vertical, 4)
-            
+        TrackableScrollView(header: {
             ScrollView{
-                ForEach($tempBoasts, id: \.self) { boast in
-                    BoastCardView(tempBoast: boast)
-                        .padding(.bottom, 8)
-                }
+                    HStack {
+                        Spacer()
+                        Menu {
+                            Button("산이만 보기", action:{})
+                            Button("들이만 보기", action:{})
+                            Button("전체보기", action:{})
+                        } label: {
+                            Image(systemName: "line.3.horizontal.decrease.circle")
+                                .foregroundStyle(.black)
+                                .font(.title1Regular)
+                        }
+                        .padding(.trailing, 4)
+                        
+                        Button(action: {
+                            showSheet.toggle()
+                        }) {
+                            Image(systemName: "plus")
+                                .foregroundStyle(.black)
+                                .font(.title1Regular)
+                        }
+                        .sheet(isPresented: $showSheet) {
+                            BoastAddView()
+                        }
+                    }
+                    .background(.white)
+                    .padding(.trailing)
+//                    .padding(.vertical, 4)
+                    .border(.black)
             }
             .padding()
+        }, content: {
+            ForEach($tempBoasts, id: \.self) { boast in
+                BoastCardView(tempBoast: boast)
+                    .padding(.bottom, 8)
+            }
+            .border(.black)
+        })
+    }
+    
+    struct TrackableScrollView<Header: View, Content: View>: View {
+     @State private var canShowHeader = true // on of the header
+     let header: () -> Header
+     let content: () -> Content
+     var body: some View {
+      VStack {
+       if canShowHeader {
+        VStack {
+         header() // header content
         }
+        .transition(
+         .asymmetric(
+          insertion: .push(from: .top),
+          removal: .push(from: .bottom)
+         )
+        )
+       }
+       GeometryReader { outerGeo in
+        let outerHeight = outerGeo.size.height
+        ScrollView(.vertical) {
+         content() // scorllable content
+          .background {
+           GeometryReader { innnerGeo in
+            let contentHeight = innnerGeo.size.height
+            let minY = max(
+             min(0, innnerGeo.frame(in: .named("ScrollView")).minY),
+             outerHeight - contentHeight
+            )
+            Color.clear
+             .onChange(of: minY) { oldVal, newVal in
+              if (canShowHeader && newVal < oldVal) || !canShowHeader && newVal > oldVal {
+               canShowHeader = newVal > oldVal
+              }
+             }
+           }
+          }
+        }
+        .coordinateSpace(name: "ScrollView") // coordinator space
+       }
+       .padding(.top, 1)
+      }
+      .animation(.easeInOut, value: canShowHeader)
+     }
     }
 }
-
+    
 #Preview {
     BoastMainView()
 }
