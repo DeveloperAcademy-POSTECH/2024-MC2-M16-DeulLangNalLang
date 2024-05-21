89065fac15bb18b42e8039bc4ebfb8f01365c8c9
HyunJaeyeon <bomul0109@gmail.com>
2024-05-21 23:46:21 +0900

    feat: 키보드 툴바에 갤러리 추가

--- DeulLangNalLang/DeulLangNalLang/Sources/View/BoastTab/BoastAddView.swift
+++ DeulLangNalLang/DeulLangNalLang/Sources/View/BoastTab/BoastAddView.swift
@@ -3,15 +3,15 @@ import UIKit
 
 struct BoastAddView: View {
     @State private var text = ""
-    @State private var isShowingImagePicker = false
+    @State private var isShowingCamera = false
+    @State private var isShowingGallery = false
     @State private var selectedImages: [UIImage?] = [nil, nil]
     @Environment(\.presentationMode) var presentationMode
-
+    
     var body: some View {
         NavigationView {
             VStack(spacing: 0) {
-                CustomTextView(text: $text, isShowingImagePicker: $isShowingImagePicker, placeholder: "내용을 입력하세요")
-                    .frame(height: 200)
+                CustomTextView(text: $text, isShowingImagePicker: $isShowingCamera, isShowingGallery: $isShowingGallery, placeholder: "내용을 입력하세요")
                     .padding()
                 HStack {
                     Spacer()
@@ -31,7 +31,7 @@ struct BoastAddView: View {
                                     .frame(width: 177, height: 116)
                                     .clipped()
                                     .cornerRadius(16)
-
+                                
                                 VStack(spacing: 0) {
                                     HStack(spacing: 0) {
                                         Spacer()
@@ -48,21 +48,22 @@ struct BoastAddView: View {
                                 }
                             }
                             .frame(width: 177, height: 116)
-                        } 
-                            else {
+                        }
+                        else {
                             Button(action: {
-                                isShowingImagePicker = true
+                                isShowingCamera = true
                             }) {
                                 Image(systemName: "add")
                                     .font(.title)
                             }
-                            .sheet(isPresented: $isShowingImagePicker) {
-                                ImagePicker(selectedImage: $selectedImages[index])
+                            .sheet(isPresented: $isShowingCamera) {
+                                ImagePicker(selectedImage: $selectedImages[index],
+                                            isShowingGallery: $isShowingGallery)
                             }
                         }
                     }
                 }
-
+                
                 Spacer()
             }
             .navigationBarTitle("자랑쓰기")
@@ -77,17 +78,18 @@ struct BoastAddView: View {
 struct CustomTextView: UIViewRepresentable {
     @Binding var text: String
     @Binding var isShowingImagePicker: Bool
+    @Binding var isShowingGallery: Bool
     var placeholder: String
-
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-
+    
     func makeUIView(context: Context) -> UITextView {
         let textView = UITextView()
         textView.delegate = context.coordinator
         textView.font = UIFont.systemFont(ofSize: 16)
-
+        
         // Placeholder 설정
         textView.text = placeholder
         textView.textColor = .lightGray
@@ -95,14 +97,23 @@ struct CustomTextView: UIViewRepresentable {
         // 툴바 설정
         let toolbar = UIToolbar()
         toolbar.sizeToFit()
-        let cameraButton = UIBarButtonItem(barButtonSystemItem: .camera, target: context.coordinator, action: #selector(context.coordinator.cameraButtonTapped))
+        let cameraButton = UIBarButtonItem(barButtonSystemItem: .camera, target: context.coordinator,
+            action: #selector(context.coordinator.cameraButtonTapped)
+        )
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        toolbar.items = [cameraButton, flexibleSpace]
+        let galleryButton = UIBarButtonItem(
+            image: UIImage(systemName: "photo"),
+            style: .plain,
+            target: context.coordinator,
+            action: #selector(context.coordinator.galleryButtonTapped)
+        )
+        
+        toolbar.items = [cameraButton, galleryButton, flexibleSpace]
         textView.inputAccessoryView = toolbar
-
+        
         return textView
     }
-
+    
     func updateUIView(_ uiView: UITextView, context: Context) {
         if text.isEmpty {
             uiView.text = placeholder
@@ -112,41 +123,48 @@ struct CustomTextView: UIViewRepresentable {
             uiView.textColor = .black
         }
     }
-
+    
     class Coordinator: NSObject, UITextViewDelegate {
         var parent: CustomTextView
-
+        
         init(_ parent: CustomTextView) {
             self.parent = parent
         }
-
+        
         func textViewDidBeginEditing(_ textView: UITextView) {
             if textView.textColor == .lightGray {
                 textView.text = ""
                 textView.textColor = .black
             }
         }
-
+        
         func textViewDidEndEditing(_ textView: UITextView) {
             if textView.text.isEmpty {
                 textView.text = parent.placeholder
                 textView.textColor = .lightGray
             }
         }
-
+        
         func textViewDidChange(_ textView: UITextView) {
             parent.text = textView.text
         }
-
+        
         func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
             let currentText = textView.text ?? ""
             guard let stringRange = Range(range, in: currentText) else { return false }
             let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
             return updatedText.count <= 100
         }
-
+        
         @objc func cameraButtonTapped() {
             parent.isShowingImagePicker = true
+            parent.isShowingGallery = true
+        }
+        
+        //MARK: 갤러리 버튼 tap 했을 때
+        @objc func galleryButtonTapped() {
+            parent.isShowingImagePicker = true
+            parent.isShowingGallery = false
         }
     }
 }
@@ -154,35 +172,39 @@ struct CustomTextView: UIViewRepresentable {
 struct ImagePicker: UIViewControllerRepresentable {
     @Environment(\.presentationMode) private var presentationMode
     @Binding var selectedImage: UIImage?
-
+    @Binding var isShowingGallery: Bool
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-
+    
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
-        picker.sourceType = .camera
+        //MARK: camera / gallery 설정
+        if isShowingGallery {
+            picker.sourceType = .camera
+        }
         return picker
     }
-
+    
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
-
+    
     class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
         var parent: ImagePicker
-
+        
         init(_ parent: ImagePicker) {
             self.parent = parent
         }
-
+        
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let image = info[.originalImage] as? UIImage {
                 parent.selectedImage = image
             }
-
+            
             parent.presentationMode.wrappedValue.dismiss()
         }
-
+        
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             parent.presentationMode.wrappedValue.dismiss()
         }
