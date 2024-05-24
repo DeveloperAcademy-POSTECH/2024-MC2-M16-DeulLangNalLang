33e30545c6b9ec883bf80d51098eade54fc32cc3
ryumbrella <sani9511@naver.com>
2024-05-24 12:48:09 +0900

    fix: BoastDetailView 레이아웃 가수정

--- DeulLangNalLang/DeulLangNalLang/Sources/View/AwardTab/Detail/BoastDetailView.swift
+++ DeulLangNalLang/DeulLangNalLang/Sources/View/AwardTab/Detail/BoastDetailView.swift
@@ -1,8 +1,63 @@
+////
+////  AwardDetailView.swift
+////  DeulLangNalLang
+////
+////  Created by  SAN RYU on 5/18/24.
+////
 //
-//  AwardDetailView.swift
-//  DeulLangNalLang
+//import SwiftUI
+//import SwiftData
 //
-//  Created by  SAN RYU on 5/18/24.
+//struct BoastDetailView: View {
+//    @Environment(\.dismiss) var dismiss
+//
+//    @Query var boasts: [Boast]
+//
+//    @State private var isFavorite: Bool = false
+//
+//    var boastID: UUID
+//
+//    var boast: Boast {
+//        return boasts.first { $0.id == boastID }!
+//    }
+//
+//    var body: some View {
+//        VStack(spacing: 0){
+//            ZStack{
+//                RoundedRectangle(cornerRadius: 20)
+//                    .frame(width: 329, height: 480)
+//                    .foregroundColor(boast.writer == "류산" ? .DNBlue : .DNGreen)
+//                    .shadow(radius: 5, x: 3, y: 3)
+//
+//                VStack(spacing: 0){
+//                    HStack(spacing: 0){
+//                        ForEach(boast.imageDatas, id: \.self) { data in
+//                            if let image = UIImage(data: data) {
+//                                Image(uiImage: image)
+//                                    .resizable()
+//                                    .aspectRatio(contentMode: .fill)
+//                                    .frame(width: 136, height: 220)
+//                                    .clipped()
+//                                    .cornerRadius(8)
+//                                    .padding(.top, 20)
+//                                    .padding(.horizontal, 5)
+//                            }
+//                        }
+//                    }
+//
+//                    Text("\(boast.contents)")
+//                        .font(.bodyRegular)
+//                        .frame(width: 281, height: 152, alignment: .topLeading)
+//                        .padding(.vertical, 22)
+//                    Text("\(getDateFormat(date: boast.date))")
+//                        .font(.bodyEmphasized)
+//                        .frame(width: 281, alignment: .leading)
+//                }
+//            }
+//            .frame(width: 329, height: 480)
+//        }
+//    }
+//}
 //
 
 import SwiftUI
@@ -22,14 +77,48 @@ struct BoastDetailView: View {
     }
     
     var body: some View {
-        VStack(spacing: 0){
-            ZStack{
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 329, height: 480)
-                    .foregroundColor(boast.writer == "류산" ? .DNBlue : .DNGreen)
-                    .shadow(radius: 5, x: 3, y: 3)
+        ZStack{
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 329, height: 480)
+                .foregroundColor(boast.writer == "류산" ? .DNBlue : .DNGreen)
+                .shadow(radius: 5, x: 3, y: 3)
+            
+            VStack(spacing: 0){
                 
-                VStack(spacing: 0){
+                if boast.imageDatas.count == 0 {
+                    Text("\(boast.contents)")
+                        .font(.bodyRegular)
+                        .frame(width: 281, height: 152, alignment: .topLeading)
+                        .padding(.top, 20)
+                    Spacer()
+                    Text("\(getDateFormat(date: boast.date))")
+                        .font(.bodyEmphasized)
+                        .frame(width: 281, alignment: .leading)
+                        .padding(.bottom, 22)
+                }
+                
+                if boast.imageDatas.count == 1 {
+                    if let image = UIImage(data: boast.imageDatas[0]) {
+                        Image(uiImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 281, height: 220)
+                            .clipped()
+                            .cornerRadius(8)
+                            .padding(.top, 20)
+                    }
+                    Text("\(boast.contents)")
+                        .font(.bodyRegular)
+                        .frame(width: 281, height: 152, alignment: .topLeading)
+                        .padding(.vertical, 22)
+                    
+                    Text("\(getDateFormat(date: boast.date))")
+                        .font(.bodyEmphasized)
+                        .frame(width: 281, alignment: .leading)
+                    Spacer()
+                }
+                
+                if boast.imageDatas.count == 2 {
                     HStack(spacing: 0){
                         ForEach(boast.imageDatas, id: \.self) { data in
                             if let image = UIImage(data: data) {
@@ -44,7 +133,6 @@ struct BoastDetailView: View {
                             }
                         }
                     }
-                    
                     Text("\(boast.contents)")
                         .font(.bodyRegular)
                         .frame(width: 281, height: 152, alignment: .topLeading)
@@ -54,8 +142,7 @@ struct BoastDetailView: View {
                         .frame(width: 281, alignment: .leading)
                 }
             }
-            .frame(width: 329, height: 480)
+            .frame(width: 329, height: 480)    
         }
     }
 }
-
