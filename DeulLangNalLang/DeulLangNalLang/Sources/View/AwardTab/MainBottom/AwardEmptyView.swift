d8781c7fafbeff65c91401f13aaa90f08a624aea
Xerath <yoondj98@naver.com>
2024-05-26 09:26:46 +0900

    fix: EmptyView들 잘못된 코드 수정
    ZStack -> VStack

--- DeulLangNalLang/DeulLangNalLang/Sources/View/AwardTab/MainBottom/AwardEmptyView.swift
+++ DeulLangNalLang/DeulLangNalLang/Sources/View/AwardTab/MainBottom/AwardEmptyView.swift
@@ -2,36 +2,34 @@ import SwiftUI
 struct AwardEmptyView: View {
     
     var body: some View {
-        
         ZStack {
             // 배경 색상 설정
             Color.DNBackground
                 .edgesIgnoringSafeArea(.all)
-            
-            Spacer().frame(height: 200)
-            
-            Image("EmptyViewImage")
-                .frame(width: 32, height: 56)
-                .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 2)
-            
-            Spacer().frame(height: 16)
-            
-            // Title2/KR_Emphasized
-            Text("들랑날랑 시작하기")
-                .font(.title2Emphasized)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.black)
-                .frame(width: 361, height: 28, alignment: .top)
-            
-            Spacer().frame(height: 2)
-            
-            Text("자랑을 작성하고 \n상장을 모아보세요.")
-                .font(.bodyRegular)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.black)
-                .frame(width: 361, height: 48, alignment: .top)
-            
-            Spacer().frame(height: 262)
+            VStack {
+                Spacer().frame(height: 200)
+                
+                Image("EmptyViewImage")
+                    .frame(width: 32, height: 64)
+                    .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 2)
+                
+                Spacer().frame(height: 16)
+                
+                // Title2/KR_Emphasized
+                Text("들랑날랑 시작하기")
+                    .font(.title2Emphasized)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.black)
+                    .frame(width: 361, height: 28, alignment: .top)
+                
+                Spacer().frame(height: 2)
+                
+                Text("자랑을 작성하고 \n상장을 모아보세요.")
+                    .font(.bodyRegular)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.black)
+                    .frame(width: 361, height: 48, alignment: .top)
+            }
         }
     }
 }
--- DeulLangNalLang/DeulLangNalLang/Sources/View/BoastTab/BoastEmptyView.swift
+++ DeulLangNalLang/DeulLangNalLang/Sources/View/BoastTab/BoastEmptyView.swift
@@ -1,67 +1,64 @@
 import SwiftUI
 struct BoastEmptyView: View {
-    
     var onDismiss: () -> Void
-    @State private var showSheet: Bool = false
+    @State private var isBoastAddViewShown: Bool = false
+    
     var body: some View {
-        
         ZStack {
             // 배경 색상 설정
             Color.DNBackground
                 .edgesIgnoringSafeArea(.all)
-            
-            Spacer().frame(height: 130)
-            
-            Image("EmptyViewImage")
-              .frame(width: 32, height: 64)
-              .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 2)
-             
-            Spacer().frame(height: 18)
-            
-            // Title2/KR_Emphasized
-            Text("자랑 하나 주면 안잡아먹지!")
-
-                .font(.title2Emphasized)
-              .multilineTextAlignment(.center)
-              .foregroundColor(.black)
-              .frame(width: 361, height: 28, alignment: .top)
-            
-            Spacer().frame(height: 2)
-            
-            Text("나의 첫 번째 자랑을\n작성해보세요.")
-                     .font(.bodyRegular)
-                     .multilineTextAlignment(.center)
-                     .foregroundColor(.black)
-
-            .frame(width: 361, height: 48, alignment: .top)
-
-            Spacer().frame(height: 100)
-            
-            
-            
-
-            
-            //MARK: 자랑하러 가기 버튼
-            Button(action: {
-                showSheet.toggle()
-            }) {
-
-                HStack(alignment: .center, spacing: 4) {Text("자랑하러 가기")}
-                .padding(.horizontal, 14)
-                .padding(.vertical, 7)
-                .foregroundColor(.white)
-                .background(Color(red: 0.31, green: 0.75, blue: 1))
-                .cornerRadius(40)
-
+            VStack {
+                Spacer().frame(height: 130)
+                
+                Image("EmptyViewImage")
+                    .frame(width: 32, height: 64)
+                    .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 2)
+                
+                Spacer().frame(height: 18)
+                
+                // Title2/KR_Emphasized
+                Text("자랑 하나 주면 안잡아먹지!")
+                
+                    .font(.title2Emphasized)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.black)
+                    .frame(width: 361, height: 28, alignment: .top)
+                
+                Spacer().frame(height: 2)
+                
+                Text("나의 첫 번째 자랑을\n작성해보세요.")
+                    .font(.bodyRegular)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.black)
+                
+                    .frame(width: 361, height: 48, alignment: .top)
+                
+                Spacer().frame(height: 100)
+                
+                
+                
+                
+                
+                //MARK: 자랑하러 가기 버튼
+                Button(action: {
+                    isBoastAddViewShown.toggle()
+                }) {
+                    
+                    HStack(alignment: .center, spacing: 4) {Text("자랑하러 가기")}
+                        .padding(.horizontal, 14)
+                        .padding(.vertical, 7)
+                        .foregroundColor(.white)
+                        .background(Color(red: 0.31, green: 0.75, blue: 1))
+                        .cornerRadius(40)
+                    
+                }
+                .sheet(isPresented: $isBoastAddViewShown, onDismiss: {
+                    onDismiss()
+                }) {
+                    BoastAddView(isBoastAddViewShown: $isBoastAddViewShown)
+                }
             }
-            .sheet(isPresented: $showSheet, onDismiss: {
-                onDismiss()
-            }) {
-                BoastAddView()
-            }
-            
-//            Spacer().frame(height: 32)
-            
         }
     }
 }
