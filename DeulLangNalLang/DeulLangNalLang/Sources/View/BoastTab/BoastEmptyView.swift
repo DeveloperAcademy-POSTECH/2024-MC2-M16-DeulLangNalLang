50ccc55a441b28c54c6c22ab4fe49b9f7e6c154d
ryumbrella <sani9511@naver.com>
2024-06-12 15:51:26 +0900

    fix: 자랑 엠티뷰 로고 및 텍스트 수정, 기존 버튼 제거, 스크롤뷰 구현

--- DeulLangNalLang/DeulLangNalLang/Sources/View/BoastTab/BoastEmptyView.swift
+++ DeulLangNalLang/DeulLangNalLang/Sources/View/BoastTab/BoastEmptyView.swift
@@ -16,57 +16,63 @@ struct BoastEmptyView: View {
             // 배경 색상 설정
             Color.DNBackground
                 .edgesIgnoringSafeArea(.all)
-            VStack {
-                Spacer().frame(height: 200)
-                
-                Image("EmptyViewImage")
-                    .frame(width: 32, height: 64)
-                    .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 2)
-                
-                Spacer().frame(height: 18)
-                
-                // Title2/KR_Emphasized
-                Text("자랑 하나 주면 안잡아먹지!")
-                
-                    .font(.title2Emphasized)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.black)
-                    .frame(width: 361, height: 28, alignment: .top)
-                
-                Spacer().frame(height: 2)
-                
-                Text("즐거웠던 일을\n마음껏 자랑해보세요.")
-                    .font(.bodyRegular)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.black)
-                
-                    .frame(width: 361, height: 48, alignment: .top)
-                
-                Spacer().frame(height: 100)
-                
-                
-                
-                
-                
-                //MARK: 자랑하러 가기 버튼
-                Button(action: {
-                    isBoastAddViewShown.toggle()
-                }) {
+            ScrollView{
+                VStack(spacing: 0){
                     
-                    HStack(alignment: .center, spacing: 4) {Text("자랑하러 가기")}
-                        .padding(.horizontal, 14)
-                        .padding(.vertical, 7)
-                        .foregroundColor(.white)
-                        .background(Color(red: 0.31, green: 0.75, blue: 1))
-                        .cornerRadius(40)
+                    HStack {
+                        Text("류들님\n새로운 자랑을 올려보세요")
+                            .font(.title1Emphasized)
+                            .fontWeight(.heavy)
+                            .padding(.bottom, 20)
+                            .padding(.horizontal)
+                        Spacer()
+                    }
+                    Spacer().frame(height: 130)
                     
+                    Image("EmptyViewimage")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 85, height: 136)
+                        .saturation(0)
+                        .contrast(1.5)
+                    //                Spacer().frame(height: 5)
+                    
+                    Text("들랑날랑하기")
+                    
+                        .font(.title2Emphasized)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.gray)
+                        .frame(width: 361, height: 28, alignment: .top)
+                    
+                    Spacer().frame(height: 2)
+                    
+                    Text("자랑을 작성하고 상장을 모아보세요.")
+                        .font(.bodyRegular)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.gray)
+                        .frame(width: 361, height: 22, alignment: .top)
+                    
+                    //MARK: 자랑하러 가기 버튼
+                    //                Button(action: {
+                    //                    isBoastAddViewShown.toggle()
+                    //                }) {
+                    //
+                    //                    HStack(alignment: .center, spacing: 4) {Text("자랑하러 가기")}
+                    //                        .padding(.horizontal, 14)
+                    //                        .padding(.vertical, 7)
+                    //                        .foregroundColor(.white)
+                    //                        .background(Color(red: 0.31, green: 0.75, blue: 1))
+                    //                        .cornerRadius(40)
+                    //
+                    //                }
+                    //                .sheet(isPresented: $isBoastAddViewShown, onDismiss: {
+                    //                    onDismiss()
+                    //                }) {
+                    //                    BoastAddView(isBoastAddViewShown: $isBoastAddViewShown)
+                    //                }
+                    
+                    Spacer()
                 }
-                .sheet(isPresented: $isBoastAddViewShown, onDismiss: {
-                    onDismiss()
-                }) {
-                    BoastAddView(isBoastAddViewShown: $isBoastAddViewShown)
-                }
-                Spacer()
             }
         }
     }
